3f5bebf83dac3461cda4296654a1ae4f99d5969c
chaiyd <chaiyd.cn@gmail.com>
2022-01-18 12:01:10 +0800

    openvpn 2.5.4

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
-FROM alpine:3.11
+FROM alpine:3.15
 
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
     && apk add tzdata \
     && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
     && echo "Asia/Shanghai" > /etc/timezone \
     && apk del tzdata \
-    && apk add dumb-init openvpn openvpn-auth-ldap openssl-dev \
+    && apk add dumb-init bash openvpn easy-rsa openvpn-auth-ldap openssl openssl-dev \
     && rm -rf /var/cache/apk/*
 
-ADD openvpn /etc/openvpn
-ADD start.sh /
+RUN ln -s /usr/share/easy-rsa/easyrsa /usr/bin/easyrsa
+ADD conf /etc/openvpn
+ADD start.sh /etc/openvpn
 
 ENTRYPOINT ["/usr/bin/dumb-init", "--"]
-CMD ["/bin/sh","/start.sh"]
+CMD ["/bin/sh","/etc/openvpn/start.sh"]
--- start.sh
+++ start.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 
-mkdir /dev/net
-mknod /dev/net/tun c 10 200
+#mkdir /dev/net
+#mknod /dev/net/tun c 10 200
 
-openvpn --config /etc/openvpn/server.conf
+#if [ ! -d /etc/openvpn/key ]; then
+if [ "`ls /etc/openvpn/key`" = "" ]; then
+        cat <<EOF > /usr/share/easy-rsa/vars
+         ##证书有效期
+         set_var EASYRSA_CA_EXPIRE	3650
+         set_var EASYRSA_CERT_EXPIRE	3650
+EOF
 
-tail -f /etc/openvpn/server-openvpn.log 
+        easyrsa init-pki
+	easyrsa --batch build-ca nopass
+	easyrsa build-server-full server nopass
+	easyrsa build-client-full client nopass
+	easyrsa gen-dh
+	mkdir /etc/openvpn/key
+        openvpn --genkey secret /etc/openvpn/key/ta.key	
+	cp pki/private/server.key /etc/openvpn/key/
+        cp pki/issued/server.crt /etc/openvpn/key/ 
+	cp pki/ca.crt /etc/openvpn/key/
+	cp pki/dh.pem /etc/openvpn/key/
+	#cp -r pki /etc/openvpn/
+        exec openvpn --config /etc/openvpn/server.conf
+else
+	exec openvpn --config /etc/openvpn/server.conf
+fi
